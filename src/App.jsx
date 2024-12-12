5b72cbef490bcda7bbe942f20bdc09fcee1d7949
Anastasiya.Honcharyk <honcharyk19@gmail.com>
2024-12-12 17:24:28 -0600

    fixed errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route  path="/" element={  <Home />} />
-      </Routes>
-    </Router>
+   <div>
+    <Header/>
+    <Home/>
+   </div>
   );
 }
 
